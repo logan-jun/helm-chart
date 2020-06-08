fa7d0ed3f0fb6a933e55d63872bef26f55df081a
병선 Byungsun <57519989+logan-jun@users.noreply.github.com>
2020-06-08 15:42:44 +0900

    test1

--- resources/config/bootstrap.sh
+++ resources/config/bootstrap.sh
@@ -38,10 +38,9 @@ if [[ $2 == "datanode" ]]; then
     $HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
 fi
 
-if [[ "${HOSTNAME}" =~ "yarn-rm" ]]; then
+if [[ $2 == "yarn-rm" ]]; then
   cp ${CONFIG_DIR}/start-yarn-rm.sh $HADOOP_HOME/sbin/
   cd $HADOOP_HOME/sbin
-  mkdir test1
   chmod +x start-yarn-rm.sh
   ./start-yarn-rm.sh
 fi
